0da8b7b00cc9282ec66f6987dc754c290e7844e4
Jürgen Gipser <juergen.gipser@continental-corporation.com>
2024-10-09 10:51:11 +0200

    add my first hello world module

---
+++ JuergenGipser/helloWorld.cpp
@@ -0,0 +1,21 @@
+// MyHelloWorld.cpp : This file contains the 'main' function. Program execution begins and ends there.
+#include <iostream>
+
+int main()
+{
+    std::cout << "hello world" << std::endl;
+
+    return 0;
+}
+
+// Change to your repository folder where your program files are located.
+// Note: I used location: "D:\repos_mine\hello-world\JuergenGipser"
+// Open Windows console in your repository folder: "cmd"
+// Run "code ." in the Windows console command prompt
+// Open Terminal in VSCode menu: View=>Terminal
+// Run command "g++ .\helloWorld.cpp -o  helloWorld.exe" or
+//             "g++ .\*.cpp -o  helloWorld.exe" in case you have more modules to compile and link.
+// Find more info on: https://code.visualstudio.com/docs/cpp/config-mingw
+// Not working yet:
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
